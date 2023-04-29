91aaab66619f53f4322ea4f257c744afd01713c0
PrathmTayade <122280972+PrathmTayade@users.noreply.github.com>
2023-04-30 02:14:36 +0530

    env

--- frontend/src/redux/apis/api.js
+++ frontend/src/redux/apis/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5001/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
   tagTypes: ["Topics", "Sectors"],
   endpoints: (build) => ({
     getTopics: build.query({
